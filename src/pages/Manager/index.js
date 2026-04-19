8bb160ac1b5bf56b3bd731c259624f5922532d1a
MinhPhm <134669344+minhpham1909@users.noreply.github.com>
2026-04-19 18:07:28 +0700

    feat: update UserMenu to conditionally show profile based on user role and add barrel exports for Manager pages

---
+++ src/pages/Manager/index.js
@@ -0,0 +1,11 @@
+/**
+ * pages/Manager/index.js
+ * Barrel exports for Manager pages.
+ */
+
+export { default as ManagerStatisticsPage } from './ManagerStatisticsPage';
+export { default as ManagerPostsPage } from './ManagerPostsPage';
+export { default as ManagerWalletPage } from './ManagerWalletPage';
+export { default as ManagerPrivacyPage } from './ManagerPrivacyPage';
+export { default as ManagerFeedbackPage } from './ManagerFeedbackPage';
+export { default as ManagerMarketingImagesPage } from './ManagerMarketingImagesPage';
